51df90140fda07ae4f87574f605e5ae3c1099545
steve.whitmore <steve@swhitmore.com>
2019-09-06 09:08:10 -0500

    updated api folder name

--- src/app/_shared/enums/url.enum.ts
+++ src/app/_shared/enums/url.enum.ts
@@ -1,6 +1,6 @@
 export class Url {
 
-    public static API_CONTEXT = 'http://localhost/services/';
+    public static API_CONTEXT = 'http://localhost/api/';
     public static WIKI_CONTEXT = Url.API_CONTEXT + 'wiki/';
 
 }
