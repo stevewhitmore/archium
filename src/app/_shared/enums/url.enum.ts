e6786a195541a12b6500eb87259d1d6534bd57d6
steve.whitmore <steve@swhitmore.com>
2019-09-12 23:04:53 -0500

    Added 'tags' api context

--- src/app/_shared/enums/url.enum.ts
+++ src/app/_shared/enums/url.enum.ts
@@ -2,5 +2,6 @@ export class Url {
 
     public static API_CONTEXT = 'http://localhost/api/';
     public static WIKI_CONTEXT = Url.API_CONTEXT + 'wiki/';
+    public static TAGS_CONTEXT = Url.API_CONTEXT + 'tags/';
 
 }
