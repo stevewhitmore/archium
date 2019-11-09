8d1779534061d65fa7c34dea50f561780399ea96
steve.whitmore <steve@swhitmore.com>
2019-11-09 09:26:54 -0600

    Added a few happy path tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -33,4 +33,40 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('isolated tests', () => {
+    it('should call authenticationService.logOut()', () => {
+      const spy = spyOn(authenticationServiceStub, 'logOut');
+
+      component.logOut();
+
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should have loginModalOn off by default', () => {
+      expect(component.loginModalOn).toBe(false);
+    });
+
+    it('should toggle off login modal', () => {
+      component.loginModalOn = true;
+
+      component.toggleLoginModalOff();
+
+      expect(component.loginModalOn).toBe(false);
+    });
+
+    it('should toggle off login modal', () => {
+      component.toggleLoginModalOn();
+
+      expect(component.loginModalOn).toBe(true);
+    });
+
+    it('should check to see if the user is logged in', () => {
+      const spy = spyOn(authenticationServiceStub, 'isLoggedIn');
+
+      component.checkLoginStatus();
+
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  });
 });
