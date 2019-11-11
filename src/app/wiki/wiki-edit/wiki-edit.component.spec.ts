217d170c513968336ffad7925a4193b6d54de921
steve.whitmore <steve@swhitmore.com>
2019-11-11 07:44:51 -0600

    happy path shallow integration tests

--- src/app/wiki/wiki-edit/wiki-edit.component.spec.ts
+++ src/app/wiki/wiki-edit/wiki-edit.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { SimpleChange } from '@angular/core';
+import { SimpleChange, DebugElement } from '@angular/core';
 
 import { WikiEditComponent } from './wiki-edit.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 const mockPage = require('../../_shared/testing/test-data/page-single.json');
 
 describe('WikiEditComponent', () => {
   let component: WikiEditComponent;
   let fixture: ComponentFixture<WikiEditComponent>;
+  let debugEl: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,6 +23,7 @@ describe('WikiEditComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WikiEditComponent);
     component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
     const changes = { pageContent: new SimpleChange(null, mockPage, true) }
     component.ngOnChanges(changes);
     fixture.detectChanges();
@@ -59,5 +62,62 @@ describe('WikiEditComponent', () => {
 
       expect(component.wikiEditForm.controls.path.value).toEqual(expectedPath);
     });
-  });
+  }); //isolated tests
+
+  describe('shallow integration tests', () => {
+    it('should be disabled if title formControl is empty', () => {
+      const saveButton = debugEl.query(By.css('#save-edit'));
+
+      component.wikiEditForm.controls.title.setValue('');
+      component.wikiEditForm.controls.path.setValue('some-path');
+      component.wikiEditForm.controls.content.setValue('some content');
+      fixture.detectChanges();
+
+      expect(saveButton.nativeElement.disabled).toBe(true);
+    });
+
+    it('should be disabled if path formControl is empty', () => {
+      const saveButton = debugEl.query(By.css('#save-edit'));
+
+      component.wikiEditForm.controls.title.setValue('Some Path');
+      component.wikiEditForm.controls.path.setValue('');
+      component.wikiEditForm.controls.content.setValue('some content');
+      fixture.detectChanges();
+
+      expect(saveButton.nativeElement.disabled).toBe(true);
+    });
+
+    it('should be enabled if title and path formControls are populated', () => {
+      const saveButton = debugEl.query(By.css('#save-edit'));
+
+      component.wikiEditForm.controls.title.setValue('Some Path');
+      component.wikiEditForm.controls.path.setValue('some-path');
+      component.wikiEditForm.controls.content.setValue('some content');
+      fixture.detectChanges();
+
+      expect(saveButton.nativeElement.disabled).toBe(false);
+    });
+
+    it('should trigger saveEdit() when clicked', () => {
+      const spy = spyOn(component, 'saveEdit');
+      const saveButton = debugEl.query(By.css('#save-edit'));
+      component.wikiEditForm.controls.title.setValue('Some Path');
+      component.wikiEditForm.controls.path.setValue('some-path');
+      component.wikiEditForm.controls.content.setValue('some content');
+      fixture.detectChanges();
+
+      saveButton.triggerEventHandler('click', null);
+
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should trigger cancelEdit() when clicked', () => {
+      const spy = spyOn(component, 'cancelEdit');
+      const cancelButton = debugEl.query(By.css('#cancel-edit'));
+
+      cancelButton.triggerEventHandler('click', null);
+
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
+  }); //shallow integration tests
 });
