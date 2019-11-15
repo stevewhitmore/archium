0ba7a709ba4aa9cf1bbce88e8489918d9f866d3c
steve.whitmore <steve@swhitmore.com>
2019-11-14 21:38:10 -0600

    Unbroke top level tags component unit tests

--- src/app/_shared/testing/stubs/index.ts
+++ src/app/_shared/testing/stubs/index.ts
@@ -1,4 +1,5 @@
 export * from './authentication-service.stub';
 export * from './notification-service.stub';
 export * from './router.stubs';
+export * from './tags-service.stub';
 export * from './wiki-service.stub';
