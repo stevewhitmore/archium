d69cdb633b57cf9aaa43f9f316f6dae1b84cad22
stevewhitmore <s25w89@gmail.com>
2020-04-03 15:28:56 -0500

    Tag stuff needs to be gutted and redone. Commented out temporarily

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -9,63 +9,64 @@ import { NotificationService } from '../_shared/notification.service';
   templateUrl: './tags.component.html',
   styleUrls: ['./tags.component.scss']
 })
-export class TagsComponent implements OnChanges {
-  @Input() pageId: string;
-  pageTags: any[];
-  allTags: any[];
-  updateMode: boolean = false;
+export class TagsComponent {
+// export class TagsComponent implements OnChanges {
+  // @Input() pageId: string;
+  // pageTags: any[];
+  // allTags: any[];
+  // updateMode: boolean = false;
 
-  constructor(private tagsService: TagsService,
-              private authenticationService: AuthenticationService,
-              private notificationService: NotificationService) {
-  }
+  // constructor(private tagsService: TagsService,
+  //             private authenticationService: AuthenticationService,
+  //             private notificationService: NotificationService) {
+  // }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.pageId.currentValue !== changes.pageId.previousValue) {
-      this.pageId = changes.pageId.currentValue;
-    }
-    this.getTagData(this.pageId);
-    this.getAllTags();
-  }
+  // ngOnChanges(changes: SimpleChanges) {
+  //   if (changes.pageId.currentValue !== changes.pageId.previousValue) {
+  //     this.pageId = changes.pageId.currentValue;
+  //   }
+  //   this.getTagData(this.pageId);
+  //   this.getAllTags();
+  // }
 
-  getTagData(pageId) {
-    this.tagsService.getTagData(pageId)
-    .pipe(take(1))
-    .subscribe(data => {
-      this.pageTags = data ? data : [];
-    });
-  }
+  // getTagData(pageId) {
+  //   this.tagsService.getTagData(pageId)
+  //   .pipe(take(1))
+  //   .subscribe(data => {
+  //     this.pageTags = data ? data : [];
+  //   });
+  // }
 
-  getAllTags() {
-    this.tagsService.getAllTags()
-    .pipe(take(1))
-    .subscribe(data => {
-      this.allTags = data ? data : [];
-    });
-  }
+  // getAllTags() {
+  //   this.tagsService.getAllTags()
+  //   .pipe(take(1))
+  //   .subscribe(data => {
+  //     this.allTags = data ? data : [];
+  //   });
+  // }
 
-  handleUpdateTagsEvent(updatedTags) {
-    if (updatedTags) {
-      this.pageTags = updatedTags;
+  // handleUpdateTagsEvent(updatedTags) {
+  //   if (updatedTags) {
+  //     this.pageTags = updatedTags;
 
-      const tagData = {
-        pageId: this.pageId,
-        tags: this.pageTags
-      }
+  //     const tagData = {
+  //       pageId: this.pageId,
+  //       tags: this.pageTags
+  //     }
 
-      this.tagsService.updateTags(tagData)
-        .pipe(take(1))
-        .subscribe(resp => {
-          this.notificationService.notify('success', resp.message)
-        });
-    }
-    this.toggleUpdateTags();
-  }
+  //     this.tagsService.updateTags(tagData)
+  //       .pipe(take(1))
+  //       .subscribe(resp => {
+  //         this.notificationService.notify('success', resp.message)
+  //       });
+  //   }
+  //   this.toggleUpdateTags();
+  // }
 
-  toggleUpdateTags() {
-    if (!this.authenticationService.isLoggedIn()) {
-      return;
-    }
-    this.updateMode = !this.updateMode;
-  }
+  // toggleUpdateTags() {
+  //   if (!this.authenticationService.isLoggedIn()) {
+  //     return;
+  //   }
+  //   this.updateMode = !this.updateMode;
+  // }
 }
--- src/app/tags/tags.module.ts
+++ src/app/tags/tags.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 import { TagsComponent } from '../tags/tags.component';
@@ -14,8 +11,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
     imports: [
         CommonModule,
-        MatAutocompleteModule,
-        MatFormFieldModule,
         MatInputModule,
         ReactiveFormsModule
     ],
@@ -31,8 +26,7 @@ import { ReactiveFormsModule } from '@angular/forms';
         TagsComponent
     ],
     providers: [
-        TagsService,
-        {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
+        TagsService
     ]
 })
 export class TagsModule {}
