6e0ce44d14f40b144d75d6961e987c88d7730741
stevewhitmore <s25w89@gmail.com>
2020-05-13 20:25:43 -0500

    A little it of styling for easier development

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './shared/page-not-found.component';
+import { WikiComponent } from './wiki/wiki.component';
 
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
     loadChildren: () => import('./wiki/wiki.module').then(m => m.WikiModule),
     data: { preload: true }
   },
+  { path: '', redirectTo: '/wiki', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
